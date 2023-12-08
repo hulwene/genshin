75610b816e83a1dfa55bee1342090b16f63ea507
hulwene <153046380+hulwene@users.noreply.github.com>
2023-12-08 10:08:39 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const logregBox = document.querySelector(".logreg-box");
+const loginLink = document.querySelector(".login-link");
+const registerLink = document.querySelector(".register-link");
+
+registerLink.addEventListener("click", () => {
+  logregBox.classList.add("active");
+});
+
+loginLink.addEventListener("click", () => {
+  logregBox.classList.remove("active");
+});
+
+function validateAndRedirect() {
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+
+  // Trim the values
+  email = email.trim();
+  password = password.trim();
+
+  // Check if all fields are filled
+  if (email === "" || password === "") {
+    alert("Please fill in all required fields.");
+    return false;
+  }
+
+  // If validation passes, redirect to the homepage
+  window.location.href = "index.html";
+  return false;
+}
